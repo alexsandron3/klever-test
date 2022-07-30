026b1eae13619f825b659c5cf44c42afd8d4a433
alexsandron3 <alexsandro060299@outlook.com>
2022-07-30 11:34:39 -0300

    feat(mongodb): :sparkles: created method to get all users
    this method will be responsible to list all users along with their informations

--- server/controller/controller.go
+++ server/controller/controller.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -24,4 +26,29 @@ func init() {
 
 	collection = client.Database("upvote").Collection("users")
 	fmt.Println("Collection is ready")
+}
+
+func GetAllUsers() []primitive.M {
+	cur, err := collection.Find(context.Background(), bson.D{{}})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var users []primitive.M
+
+	for cur.Next(context.Background()) {
+		var user bson.M
+		err := cur.Decode(&user)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		users = append(users, user)
+	}
+
+	defer cur.Close(context.Background())
+
+	return users
+
 }
