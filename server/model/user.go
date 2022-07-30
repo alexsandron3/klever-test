72fd8b26c8cee99dd339a3892d8aeec02ffd0d0c
alexsandron3 <alexsandro060299@outlook.com>
2022-07-30 17:40:32 -0300

    feat(model): :sparkles: created method to register a new vote
    this method will be responsible for upvote and downvote a user

--- server/model/user.go
+++ server/model/user.go
@@ -11,11 +11,18 @@ import (
 
 	upvote "github.com/alexsandron3/klever-test/proto"
 	"github.com/alexsandron3/klever-test/server/controller"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // I was getting an problem when trying to start server, it always returned me that model.Server do not implement upvote.UpvoteServiceServer
 type Server struct {
 	upvote.UnimplementedUpvoteServiceServer
+}
+
+type User struct {
+	ID    string `json:"_id"`
+	Name  string `json:"name"`
+	Votes int64  `json:"votes"`
 }
 
 // TO-DO = Refact this code to return all users
@@ -31,5 +38,18 @@ func (s *Server) GetAllUsers(ctx context.Context, in *upvote.GetAllRequest) (*up
 		Id:    "62e53702bb3b492963728230",
 		Name:  "alexsandro",
 		Votes: 000,
+	}, nil
+}
+
+func (s *Server) NewVote(ctx context.Context, in *upvote.NewVoteService) (*upvote.NewVoteResponse, error) {
+
+	result := controller.UpvoteUser(in.GetId())
+	var user User
+	bsonBytes, _ := bson.Marshal(result)
+	bson.Unmarshal(bsonBytes, &user)
+
+	return &upvote.NewVoteResponse{
+		Name:  user.Name,
+		Votes: user.Votes,
 	}, nil
 }
