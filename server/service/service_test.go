78916961e4dffad55d3d5e31ea24faf4d8ab9510
alexsandron3 <alexsandro060299@outlook.com>
2022-07-31 19:13:37 -0300

    test(service): :white_check_mark: Created unit tests for method CheckIfIdIsValid

--- server/service/service_test.go
+++ server/service/service_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/alexsandron3/klever-test/server/service"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func TestGetVoteValueWhenTrue(t *testing.T) {
@@ -23,5 +25,36 @@ func TestGetVoteValueWhenFalse(t *testing.T) {
 	voteValue := service.GetVoteValue(upvoteValue)
 	if voteValue != expectedVoteValue {
 		t.Errorf("Expected: %d Received: %d ", expectedVoteValue, voteValue)
+	}
+}
+
+func TestWhenIsIsInvalid(t *testing.T) {
+	id := "1111111111111111111111111"
+
+	err := service.CheckIfIdIsValid(id)
+
+	if err == nil {
+		t.Errorf("Expected to raise an error")
+	}
+}
+
+func TestIfRaisedErrorIsCorrect(t *testing.T) {
+	id := "1111111111111111111111111"
+
+	err := service.CheckIfIdIsValid(id)
+	expectedErr := status.Errorf(codes.InvalidArgument, "Invalid ID")
+	// fmt.Println(err)
+	if err.Error() != expectedErr.Error() {
+		t.Errorf("Expected to raise correct error")
+	}
+}
+
+func TestWhenIdIsValid(t *testing.T) {
+	id := "111111111111111111111111"
+
+	err := service.CheckIfIdIsValid(id)
+
+	if err != nil {
+		t.Errorf("Expected to not raise error")
 	}
 }
